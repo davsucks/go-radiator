124bf18861e3842efd60cc2eaf3fecc5744e540a
root <root@localhost.localdomain>
2016-11-28 10:18:03 -0600

    Add interval timer to refresh the scope each minute

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,8 +1,10 @@
 angular.module('radiator', ['ui.router'])
-.controller('RadiatorCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('dashboard').then(function success(response) {
-		$scope.dashboard = angular.fromJson(response.data);
-  }, function failure(response) {
-		$scope.dashboard = {};
-  });
+.controller('RadiatorCtrl', ['$scope', '$http', '$interval', function($scope, $http, $interval) {
+  $interval(function() {
+    $http.get('dashboard').then(function success(response) {
+  		  $scope.dashboard = angular.fromJson(response.data);
+    }, function failure(response) {
+		  $scope.dashboard = {};
+    });
+  }, 60000);
 }]);
